8f1234f9b532909bdcb584c12089323d201f9381
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-07-27 18:42:06 -0400

    Link added

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ print("Pedro")
 print("matt")
 
 # test commit
+
+# https://www.instructables.com/How-to-Make-a-Numerical-Integration-Program-in-Pyt/
 #https://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html
