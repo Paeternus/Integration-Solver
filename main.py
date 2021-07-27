701710452a3cf4a32c87c8506c7e7441f86980c2
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-07-27 18:44:28 -0400

    Will this work?

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ print("matt")
 
 # test commit
 
-# https://www.instructables.com/How-to-Make-a-Numerical-Integration-Program-in-Pyt/
 #https://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html
+# https://www.instructables.com/How-to-Make-a-Numerical-Integration-Program-in-Pyt/
 
 
 # can you see this - Matt
