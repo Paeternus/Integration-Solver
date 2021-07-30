4fe369cecb590713ffbb1e64aa78144aa8f5f261
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-07-29 22:58:55 -0400

    Program works for basic functions. Must specify function as return for f(x) in Integrate(N,a,b)

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-print("Pedro")
-print("matt")
-
-# test commit
-
+#######################################################################################
+# Links to Resources
 #https://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html
 # https://www.instructables.com/How-to-Make-a-Numerical-Integration-Program-in-Pyt/
-
-
-# can you see this - Matt
+#######################################################################################
+# Imports
+from math import *
+#######################################################################################
+# Start the program
+numOfSums = int(input("Enter how many times you want to sum(more times = more accurate): "))
+lowerBound = float(input("Enter the lower integration bound: "))
+upperBound = float(input("Enter the upper integraion bound: "))
+#######################################################################################
+# Functions
+def Integrate(N,a,b):
+    def f(x):
+        # change your function
+        return x**2
+    value = 0
+    value2 = 0
+    for n in range(1,N+1):
+        value += f(a+((n-(1/2)*(b-a)/N)))
+        return str(value2) # return as a string so it can print
+#######################################################################################
+# Show your answer
+print("...................................")
+print("Here is your answer: "+Integrate(numOfSums,lowerBound,upperBound))
